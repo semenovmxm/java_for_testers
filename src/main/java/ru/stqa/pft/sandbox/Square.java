cea4c33e9555789fccb8612125723112134b53d9
smxm2 <semenovmxm@gmail.com>
2020-10-22 15:43:56 +0300

    Создан проект и первые классы

---
+++ src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+    double l;
+
+    public Square(int l) {
+        this.l = l;
+    }
+    public double area(){
+        return this.l*this.l;
+    }
+}
